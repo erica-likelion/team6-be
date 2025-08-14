e3e0a0a4b9ad62d71ac5684c9ff8ad9709dc821f
Seochan Moon <moonsc1110@gmail.com>
2025-08-14 19:14:18 +0900

    feat: Add spring project structure

---
+++ src/test/java/likelion/sajaboys/temp/TempApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.sajaboys.temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
