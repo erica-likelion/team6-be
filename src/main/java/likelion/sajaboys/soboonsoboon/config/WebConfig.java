6ef732e7ec437497594fbabeb7383a61f496c23a
Seochan Moon <moonsc1110@gmail.com>
2025-08-25 09:00:00 +0900

    feat: Add CORS configuration

--- src/main/java/likelion/sajaboys/soboonsoboon/config/WebConfig.java
+++ src/main/java/likelion/sajaboys/soboonsoboon/config/WebConfig.java
@@ -1,6 +1,7 @@
 package likelion.sajaboys.soboonsoboon.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(testUserInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
 }
